38f6f95b7d3f99600eed3cd102b3a5038f27a11e
SairamPabbisetty <sairampabbisetty04@gmail.com>
2024-01-12 13:11:29 +0530

    created ImageBlocks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import ImageBlocks from "./components/ImageBlocks";
 
 function App() {
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=41774322-47fbfc64724a580b308eec5b6&q=${term}&image_type=photo&pretty=true`)
+    .then((res) => res.json())
+    .then((data) => {
+      setImages(data.hits);
+      setIsLoading(false);
+    })
+    .catch((err) => console.log(err));
+  }, [])
+
   return (
-    <div className="text-3xl">
-      <h1>Hello World</h1>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+          {images.map(image => {
+              <ImageBlocks key={image.id} image={image}/>
+          })}
+      </div>
     </div>
   );
 }
